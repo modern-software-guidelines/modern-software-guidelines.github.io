a05ad41afaf78add4af4099d1a18bb656cbae5e3
Zach C. <fxchip@gmail.com>
2026-09-09 07:51:31 -0500

    off-by-one errors, lmao

--- guidelines/c/01-memory-and-ownership-examples/size_t_overflow_good.c
+++ guidelines/c/01-memory-and-ownership-examples/size_t_overflow_good.c
@@ -8,13 +8,14 @@ int main(int argc, char *argv[]) {
   size_t wontOverflowSize;
   size_t reqSize;
 
-  someOtherSize = SIZE_MAX-6;
-  wontOverflowSize = SIZE_MAX-7;
+  someOtherSize = SIZE_MAX-5;
+  wontOverflowSize = SIZE_MAX-6;
   reqSize = 6;
 
   printf("reqSize before overflow addition: %lu\n", reqSize);
+  printf("SIZE_MAX = %lu\n", SIZE_MAX);
 
-  if ((SIZE_MAX - someOtherSize) > reqSize) {
+  if ((SIZE_MAX - someOtherSize) >= reqSize) {
     // NOTE: the way this works is that SIZE_MAX is
     // the point at which things overflow; if 
     // SIZE_MAX - addend is *less than* the base
@@ -25,7 +26,7 @@ int main(int argc, char *argv[]) {
     printf("reqSize overflow (1)! not adding!\n");
   }
 
-  if ((SIZE_MAX - wontOverflowSize) > reqSize) {
+  if ((SIZE_MAX - wontOverflowSize) >= reqSize) {
     reqSize += wontOverflowSize;
   } else {
     printf("reqSize overflow (2)! not adding!\n");
